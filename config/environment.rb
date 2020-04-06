202972e8e33815eb5b8c3047ba84abdc6a6cf139
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-04-06 17:12:22 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,17 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
  
-ActiveRecord::Base.establish_connection(
+ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
