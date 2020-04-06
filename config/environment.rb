5f5c147d2c336c748c017da163ab8465001c103a
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-04-06 17:21:32 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,12 +13,3 @@ Bundler.require
   :database => "db/artists.sqlite"
 )
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
